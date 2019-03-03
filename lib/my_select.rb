760350d10a970dc388712cd525fe0d7317c7b11e
Daniel Schneider <daschne8@gmail.com>
2019-03-03 00:33:31 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   new_array = 0
   while i < collection.length
-    if (yield collection[i].even?)
+    if (yield collection[i])
       new_array << collection[i]
     end
     i += 1
